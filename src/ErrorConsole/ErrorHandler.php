68fdf13d2b47d6866e0e4a6f07409382fc47328e
Cláudio Silva <claudio.silva@impactwave.com>
2016-12-06 16:16:52 +0000

    Improved error handling.

--- src/ErrorConsole/ErrorHandler.php
+++ src/ErrorConsole/ErrorHandler.php
@@ -1,6 +1,7 @@
 <?php
 namespace PhpKit\WebConsole\ErrorConsole;
 
+use Electro\Exceptions\ExceptionWithTitle;
 use PhpKit\WebConsole\ErrorConsole\Exceptions\PHPError;
 
 /**
@@ -26,19 +27,23 @@ class ErrorHandler
     throw new PHPError($errstr, 0, $errno, $errfile, $errline, null, $errcontext);
   }
 
-  public static function globalExceptionHandler ($exception)
+  public static function globalExceptionHandler ($e)
   {
     $handled = false;
     if (ErrorConsole::$devEnv) {
-      ErrorConsole::display ($exception);
+      ErrorConsole::display ($e);
       $handled = true;
     }
     if (self::$nextExceptionHandler)
-      call_user_func (self::$nextExceptionHandler, $exception);
+      call_user_func (self::$nextExceptionHandler, $e);
     if (!$handled) {
       @ob_end_clean ();
-      echo "<style>body{background:silver}table {font-family:Menlo,sans-serif;font-size:12px}</style>";
-      throw $exception;
+      echo "<style>body,table{font-family:Menlo,sans-serif;font-size:12px}</style>";
+      if ($e instanceof ExceptionWithTitle)
+        echo "<h3>{$e->getTitle()}</h3>";
+      printf ("<p>%s", $e->getMessage ());
+      // Commented to prevent the display of sensitive information:
+//      printf ("<p>%s<p>At <b>%s</b>, line <b>%d</b><p>Stack trace:<p><pre>%s", $e->getFile (), $e->getLine (), $e->getTraceAsString ());
     }
     exit;
   }
@@ -50,16 +55,19 @@ class ErrorHandler
     register_shutdown_function ([static::class, 'onShutDown']);
 
     if (extension_loaded ('xdebug')) {
-      ini_set ('xdebug.collect_params',
-        1);            //[0..4] collect the parameters passed to functions when a function call is recorded
-      ini_set ('xdebug.collect_vars',
-        0);              //gather information about which variables are used in a certain scope
+      //[0..4] collect the parameters passed to functions when a function call is recorded
+      ini_set ('xdebug.collect_params', 1);
+      //gather information about which variables are used in a certain scope
+      ini_set ('xdebug.collect_vars', 0);
       ini_set ('xdebug.dump_globals', 0);
-      ini_set ('xdebug.var_display_max_children', 99); //how many array keys and object's properties are shown
-      ini_set ('xdebug.var_display_max_depth', 5);     //how many nested levels of array elements and object properties
-      ini_set ('xdebug.var_display_max_data',
-        512);    //maximum string length that is shown when variables are displayed
-      ini_set ('xdebug.max_nesting_level', 500);       //maximum level of nested functions that are allowed
+      //how many array keys and object's properties are shown
+      ini_set ('xdebug.var_display_max_children', 99);
+      //how many nested levels of array elements and object properties
+      ini_set ('xdebug.var_display_max_depth', 5);
+      //maximum string length that is shown when variables are displayed
+      ini_set ('xdebug.var_display_max_data', 512);
+      //maximum level of nested functions that are allowed
+      ini_set ('xdebug.max_nesting_level', 500);
     }
   }
 
